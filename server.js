812f6d4fbdebf1f79647d720c9286d0c0d1396fc
Jijo <jijoraju11@gmail.com>
2016-09-26 13:02:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/article-one', function(req, res){
+  res.sendFile(path.join(-dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/ui/article-two', function(req, res){
+  res.sendFile(path.join(-dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/ui/article-three', function(req, res){
+  res.sendFile(path.join(-dirname, 'ui', 'article-three.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
