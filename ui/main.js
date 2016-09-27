a7280cb65fd057741bdd35ae99c37021ac0d2f08
Jijo <jijoraju11@gmail.com>
2016-09-27 21:30:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 
 //change the text of the  div element
 var element = document.getElementById('main-text');
-element.innerHTML("Content has been updated using javascript");
+element.innerHTML="Content has been updated using javascript";
