816713f85731d523cbb5f317794a5f7a63fa23d9
Jijo <jijoraju11@gmail.com>
2016-09-27 18:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ var articleOne = {
 };
 
 function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
     var htmlTemplate = `<html>
     <head>
         <title>
@@ -47,6 +51,7 @@ function createTemplate(data){
     
     </html>
     `;
+    return htmlTemplate;
 }
 
 
@@ -54,7 +59,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
