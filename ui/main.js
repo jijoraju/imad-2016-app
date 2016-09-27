fdf932f9af5bc6400c58ebd458358fd22ccd2517
Jijo <jijoraju11@gmail.com>
2016-09-27 22:40:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ element.innerHTML="Content has been updated using javascript";
 var img = document.getElementById('jijo-pic');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
