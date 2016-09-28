370240eec54a1cfccfa27343e0a8cd94ff70d456
Jijo <jijoraju11@gmail.com>
2016-09-28 21:36:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,3 @@
-console.log('Loaded!');
+//counter endpoint
 
-//change the text of the  div element
-var element = document.getElementById('main-text');
-element.innerHTML="Content has been updated using javascript";
-
-//animate the image
-var img = document.getElementById('jijo-pic');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-};
+var button = document.getElementById('counter');
