2fefdc74f3274b13d87fa1392fad2cc78ca3d295
Jijo <jijoraju11@gmail.com>
2016-09-28 21:39:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 //counter endpoint
 
-var button = document.getElementById('counter');
+var button = document.getElementById('click');
+var counter=0;
+
+button.onclick = function(){
+    
+    
+    
+    counter=counter+1;
+    var count=document.getElemetById('count');
+    count.innerHTML= counter.toString();
+};
