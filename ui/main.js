02e37904d2119a8af16d86e958050a40a673fbb5
Jijo <jijoraju11@gmail.com>
2016-09-28 21:58:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,26 @@ var counter = 0;
 
 button.onclick = function(){
 
-    counter=counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
+    //create request object
+    var request = new XMLHttpRequest();
+    
+    //get the response and store it
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            // action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML= counter.toString();
+            }
+            
+        }
+    };
+    // make request
+    
+    request.open('GET', 'http://jijoraju.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+    
+    
 };
